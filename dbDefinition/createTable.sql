3c96b1230cd55bf4235017e513994c1ffa4a90c2
Jordan Beaumont <Jordan.Beaumont@bbd.co.za>
2024-02-08 20:41:00 +0200

    added CREATE TABLE for ApprovalStatusIndex

--- dbDefinition/createTable.sql
+++ dbDefinition/createTable.sql
@@ -3,3 +3,9 @@ CREATE TABLE [dbo].[BBDTotalAllocation]
   YearId [int] UNIQUE,
   Amount [money]
 );
+
+CREATE TABLE [dbo].[ApprovalStatusIndex]
+(
+  StatusID [int] IDENTITY(1,1),
+  Status [varchar](8)
+);
