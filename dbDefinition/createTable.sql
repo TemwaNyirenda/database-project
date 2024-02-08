8f501b4ea5eeebbed67e99423b443095c14e6abb
Jordan Beaumont <Jordan.Beaumont@bbd.co.za>
2024-02-08 20:47:26 +0200

    added CREATE TABLE for StudentInfo

--- dbDefinition/createTable.sql
+++ dbDefinition/createTable.sql
@@ -9,3 +9,17 @@ CREATE TABLE [dbo].[ApprovalStatusIndex]
   StatusID [int] IDENTITY(1,1),
   Status [varchar](8)
 );
+
+CREATE TABLE [dbo].[StudentInfo]
+(
+  StudentID [int] IDENTITY(1,1),
+  HODID [int],
+  IDNumber [varchar](13),
+  Race [varchar],
+  StudentFirstName [varchar](100),
+  StudentLastName [varchar](100),
+  FinalYear [binary](1),
+  Motivation [varchar](1000),
+  Transcript [varchar](1000),
+  StudentStatus [int],
+);
