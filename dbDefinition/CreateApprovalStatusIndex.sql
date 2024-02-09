2ee04b38ff53b59bb0d74d6d629fb34374189540
Jordan Beaumont <Jordan.Beaumont@bbd.co.za>
2024-02-09 09:37:47 +0200

    added constraints for ApprovaStatusIndex

--- dbDefinition/CreateApprovalStatusIndex.sql
+++ dbDefinition/CreateApprovalStatusIndex.sql
@@ -1,5 +1,8 @@
 CREATE TABLE [dbo].[ApprovalStatusIndex]
 (
   StatusID [int] IDENTITY(1,1),
-  Status [varchar](8)
+  Status [varchar](8),
+  CONSTRAINT PK_StatusID PRIMARY KEY CLUSTERED ([StatusID]),
 );
+
+GO
