1dce2f3ca9d89d26cfcc9c063fdfac940bfb17bf
Jordan Beaumont <Jordan.Beaumont@bbd.co.za>
2024-02-09 08:54:35 +0200

    Create ApprovalStatusIndex moved to seerate file

---
+++ dbDefinition/CreateApprovalStatusIndex.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[ApprovalStatusIndex]
+(
+  StatusID [int] IDENTITY(1,1),
+  Status [varchar](8)
+);
--- dbDefinition/createTable.sql
+++ dbDefinition/createTable.sql
@@ -1,9 +1,3 @@
-CREATE TABLE [dbo].[ApprovalStatusIndex]
-(
-  StatusID [int] IDENTITY(1,1),
-  Status [varchar](8)
-);
-
 CREATE TABLE [dbo].[StudentInfo]
 (
   StudentID [int] IDENTITY(1,1),
