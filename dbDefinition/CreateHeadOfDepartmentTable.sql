662b9e809f70763fd29844aeaa925c412c86b8aa
TemwaNyirenda <temwanyirenda00@gmail.com>
2024-02-11 21:10:56 +0200

    Update CreateHeadOfDepartmentTable.sql

--- dbDefinition/CreateHeadOfDepartmentTable.sql
+++ dbDefinition/CreateHeadOfDepartmentTable.sql
@@ -3,12 +3,12 @@ CREATE TABLE [dbo].[Head_Of_Department] (
     [InstituteID] INT NOT NULL,
     [UserID] INT NOT NULL,
     [ContactDetailsID] INT NOT NULL,
-    CONSTRAINT PK_HeadOfDepartmentID 
+    CONSTRAINT [PK_HeadOfDepartmentID] 
         PRIMARY KEY CLUSTERED ([HeadOfDepartmentID]),
-    CONSTRAINT FK_HeadOfDepartment_InstituteID 
+    CONSTRAINT [FK_HeadOfDepartment_InstituteID] 
         FOREIGN KEY ([InstituteID])
         REFERENCES [dbo].[Institute_Info]([InstituteID]),
-    CONSTRAINT FK_HeadOfDepartment_UserID
+    CONSTRAINT [FK_HeadOfDepartment_UserID]
         FOREIGN KEY ([UserID])
         REFERENCES [dbo].[User_Details]([UserID])
 );
