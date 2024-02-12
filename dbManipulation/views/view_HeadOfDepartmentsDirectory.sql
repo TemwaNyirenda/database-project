072689f5b4b3378dcfb58845b9f752792c48d3f1
TemwaNyirenda <temwanyirenda00@gmail.com>
2024-02-12 08:23:16 +0200

    Views (currently)

---
+++ dbManipulation/views/view_HeadOfDepartmentsDirectory.sql
@@ -0,0 +1,26 @@
+CREATE VIEW vHeadOfDepartmentsDirectory AS
+SELECT 
+    [Institute_Info].[InstituteName],
+    [Person_Details].[FirstName], 
+    [Person_Details].[LastName],
+    [Person_Details].[PhoneNumber],
+    [Person_Details].[Email]
+FROM (
+    [dbo].[Institute_Info]
+    INNER JOIN
+        [dbo].[Head_Of_Department]
+    ON
+        [Head_Of_Department].[InstituteID] = [Institute_Info].[InstituteID]
+), (
+    SELECT
+        [User].[FirstName],
+        [User].[LastName],
+        [Contact_Details].[PhoneNumber],
+        [Contact_Details].[Email]
+    FROM
+        [dbo].[User_Details]
+        INNER JOIN
+        [dbo].[Contact_Details]
+        ON
+            [User_Details].[ContactDetailsID] = [Contact_Details].[ContactDetailsID]
+) Person_Details
