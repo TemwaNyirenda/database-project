2b2a8283918523ee0f7a9fee1f2fbfbddb76a870
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2024-02-12 10:55:41 +0200

    Updated ddl

--- dbDefinition/table_ContactDetails.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[Contact_Details] (
-	[ContactDetailsID] INT IDENTITY(1,1) NOT NULL,
-	[PhoneNumber] CHAR(10) NOT NULL,
-	[Email] VARCHAR(320) NOT NULL,
-	CONSTRAINT PK_ContactDetailsID 
-		PRIMARY KEY CLUSTERED ([ContactDetailsID])
-);
-GO
